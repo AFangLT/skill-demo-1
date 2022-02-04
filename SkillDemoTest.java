33782e1dfc273097da288c4217928c682212b6d6
AFangLT <a1fang@ucsd.edu>
2022-02-03 22:54:47 -0800

    forgot to save

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(0, SkillDemo.sum(1, 2));
+    }
+}
