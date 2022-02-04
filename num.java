1cd8a0d8642d9f4f0bd117fbef1220271e3396e6
AFangLT <a1fang@ucsd.edu>
2022-02-03 17:57:07 -0800

    Java file and failed test

---
+++ num.java
@@ -0,0 +1,14 @@
+public class num {
+    int num;
+
+    num(int num) {
+        this.num = num;
+    }
+
+    public boolean greaterThan(int c) {
+        if(this.num > c) {
+            return true;
+        }
+        else return false;
+    }
+}
---
+++ numTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class numTest {
+    num five = new num(5);
+    @Test
+    public void testNum() {
+        assertTrue("should fail", five.greaterThan(6));
+    }
+}
